e621725674665cc267c28675118a8152dd286bf7
Matias E. Petenatti <matiasodin96@gmail.com>
2022-03-20 17:38:37 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 function validate() {
-    let form = document.querySelector("#form")
-    let email = document.querySelector("#email").value
-    let text = document.querySelector(".text")
+  let form = document.getElementById('form');
+  let email = document.querySelector('#email').value;
+  let text = document.querySelector('.text');
 
-    let pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  let pattern =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (email.match(pattern)) {
-        form.classList.add("valid")
-        form.classList.remove("invalid")
-        text.innerHTML = "Tu email es valido"
-        text.style.color = "#00ff00"
-    } else {
-        
-        form.classList.remove("valid")
-        form.classList.add("invalid")
-        text.innerHTML = "Tu email es invalido"
-        text.style.color = "#ff0000"
-    }
+  if (email.match(pattern)) {
+    form.classList.add('valid');
+    form.classList.remove('invalid');
+    text.innerHTML = 'Tu email es valido';
+    text.style.color = '#00ff00';
+  } else {
+    form.classList.remove('valid');
+    form.classList.add('invalid');
+    text.innerHTML = 'Tu email es invalido';
+    text.style.color = '#ff0000';
+  }
 
-    if (email == " ") {
-        form.classList.remove("valid")
-        form.classList.remove("invalid")
-        text.innerHTML = ""
-    }
-
+  if (email == ' ') {
+    form.classList.remove('valid');
+    form.classList.remove('invalid');
+    text.innerHTML = '';
+  }
 }
